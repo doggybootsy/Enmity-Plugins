7ca251bc328d1fa2cd0625b8ccb6b2e1b0f0bfab
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2022-04-13 18:21:45 -0700

    ??

--- showHiddenChannels/src/index.ts
+++ showHiddenChannels/src/index.ts
@@ -1,24 +1,10 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
-import { getModuleByProps } from "enmity-api/module";
-import { after } from "enmity-api/patcher";
-
-// based off of https://github.com/xirreal-plugins/xirreal-plugins.github.io/blob/master/hiddenChannels/src/index.js
-
-const channelPerms = getModuleByProps("getChannelPermissions").default
-const {
-  Permissions, ChannelTypes
-} = getModuleByProps("ChannelTypes")
-
-let patches = []
 
 const showHiddenChannels: Plugin = {
   name: "Show Hidden Channels",
   commands: [],
   onStart() {
-    patches.push(after("showHiddenChannels", channelPerms, "can", ([permission], res) => {
-      if (Permissions.VIEW_CHANNEL === permission) return true
-      return res
-    }))
+    
   },
   onStop() {
     
