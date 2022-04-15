ca711c949b762d008908b53bf5e4509405244076
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2022-04-15 16:22:14 -0700

    help

--- showHiddenChannels/src/index.ts
+++ showHiddenChannels/src/index.ts
@@ -1,57 +1,61 @@
-import { Plugin, registerPlugin } from "enmity-api/plugins";
+import { Plugin, registerPlugin } from "enmity-api/plugins"
 import {
   create
 } from "enmity-api/patcher"
 import {
-  getModule
+  getModuleByProps
 } from "enmity-api/module"
 
-function getModuleByProps(...props:string[]):any {
-  const defaultModule = getModule(m => props.every((prop) => m.default && m.default.includes(prop)))?.default
-  if (defaultModule) return defaultModule
-  return getModule(m => props.every((prop) => m && m.includes(prop)))
-}
 
 // Fake console that sends to a WS server
 function makeConsole():Console {
   const connection:WebSocket = new WebSocket("ws://10.0.0.83:1234")
   let canLoad = false
   let didError = false
-  let delayed = []
+  const delayed = []
   connection.onopen = () => {
     canLoad = true
     for (const delayedBody of delayed) connection.send(delayedBody)
   }
   connection.onerror = () => didError = true
-  return new Proxy({}, {
-    get: (target, prop) => (...args) => {
-      if (didError) return
-      const body = {
-        time: Date.now(),
-        type: prop,
-        arguments: args,
-        delayed: false
-      }
-      if (!canLoad) {
-        body.delayed = true
-        return delayed.push(JSON.stringify(body))
-      }
-      connection.send(JSON.stringify(body))
+
+  const consoleKeys = Object.keys(console)
+
+  function send(prop:string, ...args:Array<unknown>) {
+    if (didError) return
+    const body = {
+      time: Date.now(),
+      type: prop,
+      arguments: args,
+      delayed: false
     }
-  }) as Console
+    if (!canLoad) {
+      body.delayed = true
+      return delayed.push(JSON.stringify(body))
+    }
+    connection.send(JSON.stringify(body))
+  }
+
+  const spoofedConsole = consoleKeys.map(key => [key, send.bind(this, key)])
+
+  return Object.fromEntries(spoofedConsole)
 }
 const console = makeConsole()
 
-const getChannelPermissions = getModuleByProps("getChannelPermissions")
-const unreadManager = getModuleByProps("hasUnread")
-const { getChannel } = getModuleByProps("getChannel")
-const { Permissions, ChannelTypes } = getModuleByProps("Permissions", "ChannelTypes")
+const getChannelPermissions = getModuleByProps("getChannelPermissions").default
+console.log("got getChannelPermissions")
+const unreadManager = getModuleByProps("hasUnread").default
+console.log("got unreadManager")
+const { getChannel } = getModuleByProps("getChannel").default
+console.log("got getChannel")
+const { Permissions, ChannelTypes } = getModuleByProps("Permissions")
+console.log("got Permissions")
 
 const originalCan = getChannelPermissions.can.bind({})
-
+console.log("got originalCan")
 const patcher = create("showHiddenChannels")
 
-const channelTypes = {
+const _channelTypes = {
   ignore: [
     ChannelTypes.DM, 
     ChannelTypes.GROUP_DM, 
@@ -67,18 +71,21 @@ const channelTypes = {
     ChannelTypes.PUBLIC_THREAD
   ]
 }
-// Only thing i use so
-type channelType = { type: number }
-function userCanViewChannel(channel:string|channelType) {
-  let newChannel = (channel as channelType)
+console.log("got channelTypes")
+const userCanViewChannel = (channel:string|{ type: number }) => {
+  let newChannel = (channel as { type: number })
   if (typeof channel === "string") newChannel = getChannel(channel)
-  return !newChannel || channelTypes.ignore.includes(newChannel.type) || (channelTypes.good.includes(newChannel.type) && originalCan(Permissions.VIEW_CHANNEL, newChannel))
+  if (!newChannel || _channelTypes.ignore.includes(newChannel.type)) return true
+  return _channelTypes.good.includes(newChannel.type) && originalCan(Permissions.VIEW_CHANNEL, newChannel)
 }
+console.log("got userCanViewChannel")
 
 const showHiddenChannels:Plugin = {
   name: "showHiddenChannels",
   patches: [],
   onStart() {
+    console.log("onStart")
+
     patcher.after(getChannelPermissions, "can", (_:unknown, [permissionID]:Array<number>, res:Boolean) => {
       if (permissionID === Permissions.VIEW_CHANNEL) return true
     })
@@ -92,8 +99,6 @@ const showHiddenChannels:Plugin = {
     patcher.after(unreadManager, "getMentionCount", (_:unknown, args:Array<string|unknown>, res:any) => {
       if (!userCanViewChannel(args[0] as string)) return 0
     })
-
-    console.log("onStart")
   },
   onStop() {
     console.log("onStop")
